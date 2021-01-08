643ba95b786ce3f3eeb041d84c505d9663e110d5
Александр Шавитов <shavitovad@gmail.com>
2021-01-08 12:03:19 +0300

    Added easy cipherer

---
+++ EasyCipherer/Program.cs
@@ -0,0 +1,14 @@
+﻿using Cardano.Cipherer;
+using Cardano.Commons;
+
+namespace EasyCipherer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cipherer.CipherFileWithCardano("plaintext.txt", RotationMethod.OpenAngle, GridType.Rect);
+            Cipherer.DecipherTextUsingKey("key.bin", "ciphertext.txt");
+        }
+    }
+}
